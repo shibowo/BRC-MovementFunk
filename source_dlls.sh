6e9d5fa251de2ad316da74daeaaf0e8b58ac6193
shibowo <160930430+shibowo@users.noreply.github.com>
2025-05-28 00:06:27 +0300

    much better dll grabber script

--- source_dlls.sh
+++ source_dlls.sh
@@ -1,32 +1,111 @@
 #!/bin/bash
 
-GAMEDIR=""
-MODS=""
-DLL_DEST='lib/'
-if [ $# -eq 0 ] 
+gamedirectory=""
+mods=""
+dll_dest='lib/'
+
+brc_data_managed='Bomb Rush Cyberfunk_Data/Managed/'
+bepinex_core='BepInEx/core/'
+bepinex_plugins='BepInEx/plugins'
+
+help(){
+  echo "Usage: ./source_dlls.sh [GAMEDIR] [MOD_DIR]"
+  echo "You can call this script without arguments if you have installed BRC with steam"
+  echo "and are using r2modman as your mod manager, but otherwise you'll need to define a GAMEDIR"
+  echo "and a MOD_DIR. Note that if you define GAMEDIR you also have to define MOD_DIR."
+  return 0
+}
+
+determine_mod_dir_r2mm_steam(){
+  gamedir=$2
+  #Assumes you're modding the game with r2mm.
+  r2mmprofiles="$1/.config/r2modmanPlus-local/BombRushCyberfunk/profiles/"
+  r2mmprofileslist=[]
+
+  echo "Profile List:" >&2
+  for profile in "$(ls "$r2mmprofiles")";
+  do
+    r2mmprofileslist+=("${profile}")
+    echo "${profile}" >&2
+  done
+
+  selectedprofile=""
+  isprofilevalid=0
+  read -p "Select R2MM Profile: " selectedprofile
+  for profile in "${r2mmprofileslist[@]}";
+  do
+    if [ "$profile" == "$selectedprofile" ];
+    then
+      isprofilevalid=1
+    fi
+  done
+  if [ $isprofilevalid == 1 ];
   then
-    #TODO: Figure out if game is installed in the same place as native steam, not just flatpak steam
-    GAMEDIR="$HOME/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/BombRushCyberfunk/"
-    #TODO: Don't assume default r2mm profile
-    MODS="$HOME/.var/app/com.valvesoftware.Steam/.config/r2modmanPlus-local/BombRushCyberfunk/profiles/Default/"
+    printf "$r2mmprofiles/$selectedprofile"
+  else
+    return 2
+  fi
+}
+#BRC on linux is only really possible though steam as of now
+#~/.steam/root/ should be a symlink pointing to your steam install
+
+if [ $# -eq 0 ]; 
+then
+  steamroot="$HOME/.steam/root/"
+  if [ ! -L "${steamroot}" ] && [ ! -d "${steamroot}" ];
+  then
+    printf "\033[031mYour ~/.steam/root symlink seems to be broken! Aborting..."
+    exit 1
+  fi
+  gamedir="${steamroot}steamapps/common/BombRushCyberfunk"
+  steamdir="$(readlink -e "$steamroot")"
+  steamdir+="/../../../"
+  mods="$(determine_mod_dir_r2mm_steam $steamdir $gamedir)"
+  if [ $? -eq 2 ]; 
+  then
+    printf "\033[031mProfile does not exist! Aborting..."
+    exit 2
+  fi
 else
-    GAMEDIR=$1
+  gamedir=$1
+  if [ $# -eq 2 ];
+  then
+    mods=$2
+  else
+    printf "\033[031mPlease provide a MOD_DIR! Aborting..."
+    exit 2
+  fi
+  if [ ! -d $dll_dest ];
+  then
+    mkdir -p $dll_dest
+    if [ ! -d $dll_dest ];
+    then
+      printf "\033[031mFailed to create directory \"" + $selectedprofile + "\"! Aborting..."
+      exit 3
+    fi
+    touch $dll_dest/.gitignore
+    echo '*.dll' >> $dll_dest/.gitignore
+  fi
 fi
 
-mkdir -p $DLL_DEST
+############################################
+# DLL list: add the paths to your DLLs here
+############################################
+dll_list=("$mods/BepInEx/core/0Harmony.dll")
+dll_list+=("$gamedir/Bomb Rush Cyberfunk_Data/Managed/Assembly-CSharp-firstpass.dll")
+dll_list+=("$mods/BepInEx/core/BepInEx.dll")
+dll_list+=("$mods/BepInEx/plugins/LazyDuchess-CommonAPI/CommonAPI.dll" )
+dll_list+=("$gamedir/Bomb Rush Cyberfunk_Data/Managed/UnityEngine.dll") 
+dll_list+=("$gamedir/Bomb Rush Cyberfunk_Data/Managed/UnityEngine.AnimationModule.dll") 
+dll_list+=("$gamedir/Bomb Rush Cyberfunk_Data/Managed/UnityEngine.AudioModule.dll") 
+dll_list+=("$gamedir/Bomb Rush Cyberfunk_Data/Managed/UnityEngine.CoreModule.dll") 
+dll_list+=("$gamedir/Bomb Rush Cyberfunk_Data/Managed/UnityEngine.IMGUIModule.dll") 
+dll_list+=("$gamedir/Bomb Rush Cyberfunk_Data/Managed/UnityEngine.InputLegacyModule.dll") 
+dll_list+=("$gamedir/Bomb Rush Cyberfunk_Data/Managed/UnityEngine.ParticleSystemModule.dll") 
+dll_list+=("$gamedir/Bomb Rush Cyberfunk_Data/Managed/UnityEngine.PhysicsModule.dll")
 
-cp -u $MODS/BepInEx/core/0Harmony.dll $DLL_DEST
-cp -u $GAMEDIR/'Bomb Rush Cyberfunk_Data'/Managed/Assembly-CSharp-firstpass.dll $DLL_DEST
-cp -u $GAMEDIR/'Bomb Rush Cyberfunk_Data'/Managed/Assembly-CSharp.dll $DLL_DEST #publicize this
-cp -u $MODS/BepInEx/core/BepInEx.dll $DLL_DEST
-cp -u $MODS/BepInEx/plugins/LazyDuchess-CommonAPI/CommonAPI.dll $DLL_DEST
-cp -u $GAMEDIR/'Bomb Rush Cyberfunk_Data'/Managed/UnityEngine.dll $DLL_DEST
-cp -u $GAMEDIR/'Bomb Rush Cyberfunk_Data'/Managed/UnityEngine.AnimationModule.dll $DLL_DEST
-cp -u $GAMEDIR/'Bomb Rush Cyberfunk_Data'/Managed/UnityEngine.AudioModule.dll $DLL_DEST
-cp -u $GAMEDIR/'Bomb Rush Cyberfunk_Data'/Managed/UnityEngine.CoreModule.dll $DLL_DEST
-cp -u $GAMEDIR/'Bomb Rush Cyberfunk_Data'/Managed/UnityEngine.IMGUIModule.dll $DLL_DEST
-cp -u $GAMEDIR/'Bomb Rush Cyberfunk_Data'/Managed/UnityEngine.InputLegacyModule.dll $DLL_DEST
-cp -u $GAMEDIR/'Bomb Rush Cyberfunk_Data'/Managed/UnityEngine.ParticleSystemModule.dll $DLL_DEST
-cp -u $GAMEDIR/'Bomb Rush Cyberfunk_Data'/Managed/UnityEngine.PhysicsModule.dll $DLL_DEST
+for dll in "${dll_list[@]}";
+do
+  cp -u "$dll" "$dll_dest"
+done
 
- 
